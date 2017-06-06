d9a05aa3604748a1c3077e49420d54ece2d8041a
Vilma Bezerra Alves <vilmabezerraalves@gmail.com>
2017-06-06 07:01:22 -0300

    trying Top K Words

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,13 @@ import org.apache.spark.SparkConf
 import play.api.libs.json
 
 object main {
+    def topWords(arg: Array[String]) {
+
+    }
+
+    def topSentences(){
+
+    }
     def main(arg: Array[String]) : Unit = {
         val sparkConf = (new SparkConf()).setAppName("SparkTweets").setMaster("local[4]")
         val sc = new SparkContext(sparkConf)
@@ -11,6 +18,10 @@ object main {
 
         val raw = sc.textFile("data/dataset.json")
         println(raw.first)
+        val counts = raw.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_+_)
+        counts.countByKey()
+        println(counts.first)
+
 
         sc.stop()
     }
